5d2ce5a59b98b0c8931dce94bc293eb21ea0bd29
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-01 20:36:11 +0300

    User route. Added handlers in controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ChangeAvatarUserDto } from '@src/user/dto/change-avatar-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileNameGenerator } from '@src/utilities/generators/fileNameGenerator';
+import { fileFilter } from '@src/utilities/validators/fileValidator';
 
 @Controller('user')
 export class UserController {
@@ -25,6 +40,26 @@ export class UserController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
+  }
+
+  // noinspection TypeScriptValidateTypes
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: '@data/files',
+        filename: fileNameGenerator,
+      }),
+      fileFilter: fileFilter,
+    }),
+  )
+  @Patch(':id')
+  updateAvatar(
+    @Param('id') id: string,
+    @Body() changeAvatarUserDto: ChangeAvatarUserDto,
+    @UploadedFiles() file: Express.Multer.File,
+  ) {
+    return this.userService.changeAvatar(+id, changeAvatarUserDto, file);
   }
 
   @Delete(':id')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ChangePasswordUserDto } from '@src/user/dto/change-password-user.dto';
+import { genSalt, hash } from 'bcrypt';
+import { ChangeSecretUserDto } from '@src/user/dto/change-secret-user.dto';
+import { ChangeAvatarUserDto } from '@src/user/dto/change-avatar-user.dto';
 
 @Injectable()
 export class UserService {
@@ -18,6 +22,30 @@ export class UserService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
+  }
+
+  async changePassword(
+    id: number,
+    changePasswordUserDto: ChangePasswordUserDto,
+  ) {
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(changePasswordUserDto.password, salt);
+    return `This action changes password a #${id} user`;
+  }
+
+  async changeSecret(id: number, changeSecretUserDto: ChangeSecretUserDto) {
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(changeSecretUserDto.password, salt);
+    const hashedSecret = await hash(changeSecretUserDto.secretAnswer, salt);
+    return `This action changes secret a #${id} user`;
+  }
+
+  async changeAvatar(
+    id: number,
+    changeAvatarUserDto: ChangeAvatarUserDto,
+    file: Express.Multer.File,
+  ) {
+    return `This action changes avatar a #${id} user`;
   }
 
   remove(id: number) {
