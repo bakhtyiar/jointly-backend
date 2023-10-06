ef23b9b80d4970dc55451baca54a40b6f42f531e
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-06 18:54:17 +0300

    Validation of DTOs is working now

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { MongoExceptionFilter } from '@src/utilities/filters/MongoExceptionFilter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  // todo: implement forwarding mongodb errors to response
+  // app.useGlobalFilters(new MongoExceptionFilter());
   await app.listen(process.env.PORT);
 }
 bootstrap();
