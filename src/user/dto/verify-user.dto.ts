853f963df0c5a445e47b112f292c4e6290d354a1
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-07 21:56:15 +0300

    Added dto for verifying user

---
+++ src/user/dto/verify-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsMongoId, IsOptional, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class VerifyUserDto {
+  @IsOptional()
+  @IsMongoId()
+  id: Types.ObjectId;
+
+  @IsOptional()
+  login: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsEmail()
+  email: string;
+}
