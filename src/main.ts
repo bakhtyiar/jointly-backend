efdd78bedd449681ebe0494000d427105d46699a
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-20 21:50:11 +0300

    Added appService to main.ts for setting app instance to appService

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { configKeys } from '@src/config/configuration';
+import { AppService } from '@src/app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // todo: implement forwarding mongodb errors to response
   // app.useGlobalFilters(new MongoExceptionFilter());
+
+  const appService = app.get(AppService);
+  appService.setAppInstance(app);
+
   const configService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.get(String(configKeys.PORT)));
 }
