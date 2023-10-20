34ebb001e77df38b351730e4af8ae90018270d78
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-20 21:42:03 +0300

    Added  app instance to app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private app: INestApplication;
+
+  constructor() {}
+
+  setAppInstance(app: INestApplication) {
+    this.app = app;
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getControllers() {
+    const server = this.app.getHttpServer();
+    return server._events.request._router;
   }
 }
