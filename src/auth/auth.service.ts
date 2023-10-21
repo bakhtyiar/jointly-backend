108c69099717fbb525347b1b9aa997371dee5209
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-21 22:20:53 +0300

    Added utility method to auth.service.ts which is matching global permissions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,23 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { comparePassword } from '@src/utilities/hashing/hashingPassword';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '@src/user/user.service';
 import { VerifyUserDto } from '@src/user/dto/verify-user.dto';
+import { User } from '@src/user/schemas/user.schema';
+import { Role } from '@src/roles/schemas/role.schema';
+import { PermissionDocument } from '@src/permissions/schemas/permission.schema';
+
+export type TRoleWithPermissions = Omit<Role, 'permissions'> & {
+  permissions: PermissionDocument[];
+};
+
+export type TUserWithRolesAndPermissions = Omit<User, 'roles'> & {
+  roles: TRoleWithPermissions[];
+};
 
 @Injectable()
 export class AuthService {
@@ -20,5 +35,20 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  matchGlobalPermissions(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const path = request.route.path;
+    const method = request.method;
+
+    return user.roles.find((role) => {
+      return (
+        role.permissions.some((permission) => {
+          return permission.method === method && permission.path === path;
+        }) && role.isGlobal === true
+      );
+    });
   }
 }
