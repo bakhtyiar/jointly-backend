ee3aeba646433caac32a21b98822c22a81e26711
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-21 22:20:18 +0300

    Added participant.schema.ts, it's additional schema for composition in community.schema.ts

---
+++ src/communities/schemas/participant.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Schema as SchemaMongoose, Types } from 'mongoose';
+import { User } from '@src/user/schemas/user.schema';
+import { IsString } from 'class-validator';
+
+export type ParticipantDocument = HydratedDocument<Participant>;
+
+@Schema()
+export class Participant {
+  @Prop()
+  @IsString()
+  avatar: string;
+  @Prop({
+    type: SchemaMongoose.Types.ObjectId,
+    ref: User.name,
+  })
+  userId: Types.ObjectId;
+  @Prop({ type: String })
+  communityAvatar: string;
+  @Prop({ type: String })
+  communityName: string;
+  @Prop({ type: String })
+  communityStatus: string;
+}
+
+export const ParticipantSchema = SchemaFactory.createForClass(Participant);
