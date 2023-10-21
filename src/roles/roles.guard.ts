87999896ca79867ff321491ef7ea2f7d95f83e0b
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-21 22:31:45 +0300

    Applied isPublic and globalPerm checks in roles.guard.ts

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -4,33 +4,19 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Roles } from './roles.decorator';
+import { PublicService } from '@src/public/public.service';
+import { AuthService } from '@src/auth/auth.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private publicService: PublicService,
+    private authService: AuthService,
+  ) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get(Roles, context.getHandler());
-    if (!roles) {
-      return true;
-    }
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    console.log('user', user);
-    try {
-      return this.matchRoles(roles, user.username);
-    } catch {
-      throw new UnauthorizedException();
-    }
-  }
-
-  private matchRoles(requiredRoles: string[], userRoles: string[]): boolean {
-    console.log('reqR', requiredRoles);
-    console.log('usrR', userRoles);
-    //TODO: implement vlookuped roles and find between them
-    // return requiredRoles.some((role) => userRoles.includes(role));
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    if (this.publicService.isPublic(context)) return true;
+    if (this.authService.matchGlobalPermissions(context)) return true;
+    throw new UnauthorizedException();
   }
 }
