25f91f3246cee379218efc7c68558fc73cf6bc69
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-10-22 12:58:05 +0300

    Refactored isPublic check in public.service.ts

--- src/public/public.service.ts
+++ src/public/public.service.ts
@@ -11,9 +11,13 @@ export class PublicService {
   ) {}
 
   isPublic(context: ExecutionContext): boolean {
-    return !!this.reflector.getAllAndOverride(
-      this.configService.get(configKeys.IS_PUBLIC_KEY),
-      [context.getHandler(), context.getClass()],
-    );
+    const cfgKey = this.configService.get(configKeys.IS_PUBLIC_KEY);
+    const ctxHandler = context.getHandler();
+    const ctxClass = context.getClass();
+    const result = this.reflector.getAllAndOverride(cfgKey, [
+      ctxHandler,
+      ctxClass,
+    ]);
+    return !!result;
   }
 }
